ba9fcb3a518adb37fec3b764fba84b5948585679
Jakin Chen <jchen612@fordham.edu>
2024-01-25 12:21:11 -0500

    first lean programs

--- Matroids/Basic.lean
+++ Matroids/Basic.lean
@@ -1,1 +1,20 @@
-def hello := "world"
+def sum : List Nat → Nat
+  | [] => 0
+  | head :: tail =>
+    head + sum tail
+
+
+#eval sum []
+#eval sum [1, 7, 12]
+
+def max : List Nat → Nat
+  | [] => 0
+  | head :: tail =>
+    let m := max tail
+    if m > head then
+      m
+    else
+      head
+
+
+#eval max [12, 20, 200]
