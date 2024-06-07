90187f4b4709f3cec8c69a4c7ed4c639b3055d0f
Jakin Chen <jchen612@fordham.edu>
2024-06-07 11:27:09 -0400

    Some experiements with lambda calculus

---
+++ Samples/RelabellingAux.lean
@@ -0,0 +1,8 @@
+def relabellingAux (g : Nat → Nat) (B : List Nat) : List Nat :=
+  (B.map) g
+
+def relabellingAux2 (g : Nat → Nat) : List Nat → List Nat :=
+  fun (B : List Nat)  ↦ (B.map) g
+
+def relabellingAux3 : (Nat → Nat) → List Nat → List Nat :=
+  fun g ↦ fun B ↦ (B.map) g
