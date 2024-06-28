a4f5216e5ae029e8d00f155d96aa15592684be85
Heather Macbeth <25316162+hrmacbeth@users.noreply.github.com>
2024-06-28 15:38:12 -0400

    set up daily check that the main computation still returns "39"

---
+++ Matroids/Test/MainComputation.lean
@@ -0,0 +1,13 @@
+import Matroids.MainComputation
+
+/-! # Running the main computation
+
+This file runs the main computation of the project, producing a list of the "Vámos-like" matroids.
+(There should be 39 of them, and the `#guard_msgs` command will fail if this is not the case.)
+
+This file takes a *long* time to run, about 2 hours.  It is deliberately not included in a standard
+build of the project. -/
+
+/-- info: 39 -/
+#guard_msgs in
+#eval mainComputationAux.length
