21fa0c97d8316d74182dcd20ae4321772119f9bd
Jakin Chen <jchen612@fordham.edu>
2024-06-28 12:13:33 -0400

    alphabetizing matroids.lean

--- Matroids.lean
+++ Matroids.lean
@@ -1,13 +1,10 @@
--- This module serves as the root of the `Matroids` library.
--- Import modules here that should be built as part of the library.
--- import «Matroids».Basic
 import Matroids.AllPossibilities
 import Matroids.Buckets
+import Matroids.Combinations
 import Matroids.Count
 import Matroids.NearlySame
 import Matroids.PartialMatroid
 import Matroids.Relabelling
-import Matroids.Combinations
 import Matroids.Verification.Basic
 import Matroids.Verification.Combinations
 import Matroids.Verification.Miscellaneous
