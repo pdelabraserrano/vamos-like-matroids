688ef4ae56eba293cd789248554a69b8afd8c2fc
Paulina Delabra Serrano <pau@fordham.edu>
2024-06-28 11:43:37 -0400

    added a little more description to Vamos computations

--- Matroids/Test/VamosPruning.lean
+++ Matroids/Test/VamosPruning.lean
@@ -100,27 +100,27 @@ abbrev Vamos8 := groupByBucket (augmentationsFinal 8 Vamos)
 #eval List.sum (Vamos0.map sizeOfPrunedBucket) -- returns [1] -- 1
 
 #time -- 49.753 sec
-#eval List.sum (Vamos1.map sizeOfPrunedBucket) -- returns [1] -- 2
+#eval List.sum (Vamos1.map sizeOfPrunedBucket) -- returns [1] -- + 1 = 2
 
 #time --339.491 sec / 6 min
-#eval List.sum (Vamos2.map sizeOfPrunedBucket) -- returns [1, 1, 1, 1, 1, 1] -- 8
+#eval List.sum (Vamos2.map sizeOfPrunedBucket) -- returns [1, 1, 1, 1, 1, 1] -- + 6 = 8
 
 #time -- 3538.507 sec / 1hr
-#eval (Vamos3.map sizeOfPrunedBucket) -- returns [1, 1, 5] -- 15
+#eval (Vamos3.map sizeOfPrunedBucket) -- returns [1, 1, 5] -- + 7 = 15
 
 #time -- 1471.504 sec / 25 min
-#eval (Vamos4.map sizeOfPrunedBucket) -- returns [1, 1, 1, 1, 1, 1, 1, 1, 5] -- 28
+#eval (Vamos4.map sizeOfPrunedBucket) -- returns [1, 1, 1, 1, 1, 1, 1, 1, 5] -- + 13 = 28
 
 #time -- 1016.151 sec / 17 min
-#eval (Vamos5.map sizeOfPrunedBucket) -- returns [1, 1, 3] -- 33
+#eval (Vamos5.map sizeOfPrunedBucket) -- returns [1, 1, 3] -- + 5 = 33
 
 #time -- 301.063 sec / 5 min
-#eval (Vamos6.map sizeOfPrunedBucket) -- returns [1, 1, 1, 1] -- 37
+#eval (Vamos6.map sizeOfPrunedBucket) -- returns [1, 1, 1, 1] -- + 4 = 37
 
 #time -- 97.294 sec / 1.5 min
-#eval (Vamos7.map sizeOfPrunedBucket) -- returns [1] -- 38
+#eval (Vamos7.map sizeOfPrunedBucket) -- returns [1] -- + 1 = 38
 
 #time -- 7.426 sec
-#eval (Vamos8.map sizeOfPrunedBucket) -- returns [1] -- 38
+#eval (Vamos8.map sizeOfPrunedBucket) -- returns [1] -- + 1 = 39 (desired result)
 
 #eval Vamos7.head!
