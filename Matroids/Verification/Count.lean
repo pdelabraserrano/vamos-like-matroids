f053b07983d8c6b43f76abd3e41dbd591a261a47
Heather Macbeth <25316162+hrmacbeth@users.noreply.github.com>
2024-07-04 11:54:52 -0400

    groupByValue lemma for Theorem 1

--- Matroids/Verification/Count.lean
+++ Matroids/Verification/Count.lean
@@ -1,5 +1,6 @@
 import Matroids.Verification.Basic
 import Matroids.Count
+import Matroids.PartialMatroid
 
 /- Lemma for count (related to Theorem 1):  If the input is an list of partial matroids
 (order does matter, for both the lists and for the members) with range i < n and lenght = r, then
@@ -9,7 +10,12 @@ paving matroids-/
 #eval count ([[0, 1], [0, 1], [1, 0], [0, 1, 3,2], [0, 1, 9], [0, 1], [0,2]]) /- Note, this is not
 a list of partial matroids-/
 
-
-/- Lemma for groupByValue (related to Theorem 1): If the input is an list of partial matroids
-(order does matter, for both the lists and for the members) with range i < n and lenght = r, then
-the output will be a list of lists of lawful sparse paving matroids -/
+/- If the operation `groupByValue` is performed on a list of `PartialMatroids` which are valid
+(n, r) sparse paving matroids then the `PartialMatroid`s in the output list of lists will all be
+valid (n, r) sparse paving matroids.
+(will probably get used for Theorem 1) -/
+lemma groupByValue_lawful (f : PartialMatroid → List ℕ) (A : List PartialMatroid)
+    (hA : A.Forall (fun M ↦ LawfulSparsePavingMatroid n r M.matroid)) :
+    (groupByValue A f).Forall
+    (fun l ↦ l.Forall (fun M ↦ LawfulSparsePavingMatroid n r M.matroid)) := by
+  sorry
