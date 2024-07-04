e58e182a462670a9967efbe819cd2464b76897b4
Heather Macbeth <25316162+hrmacbeth@users.noreply.github.com>
2024-07-04 11:40:47 -0400

    groupByBucket lemma for theorem 3

--- Matroids/Verification/Buckets.lean
+++ Matroids/Verification/Buckets.lean
@@ -1,12 +1,15 @@
 import Matroids.Verification.Basic
 import Matroids.Buckets
+import Matroids.Verification.Relabelling
+
+open PartialMatroid
 
 /-- If `A` is a list of `PartialMatroid`s, all of which are valid (n, r)-sparse paving matroids,
 then when the `groupByBucket` operation is performed, every `PartialMatroid` in the the resulting
 list of list of partial matroids is still a valid (n, r)-sparse paving matroids. -/
 lemma groupByBucket_lawful (A : List PartialMatroid)
     (hA : A.Forall (fun M ↦ LawfulSparsePavingMatroid n r M.matroid)) :
-    (PartialMatroid.groupByBucket A).Forall
+    (groupByBucket A).Forall
     (fun l ↦ l.Forall (fun M ↦ LawfulSparsePavingMatroid n r M.matroid)) := by
   induction A with
   | nil => simp
@@ -29,3 +32,11 @@ lemma groupByBucket_lawful (A : List PartialMatroid)
 the output will be a lawful sparse paving matroid -/
 /- After rethinking, we might not need to prove anything about countBuckets since it is not used
 directly in the main computation.-/
+
+/-- For all partial matroids in a bucket, they do not exist in other buckets even as permutations of
+partial matroids.
+(will probably get used for Theorem 3) -/
+lemma nonisomorphic_groupByBucket (A : List PartialMatroid) :
+    (groupByBucket A).Pairwise fun L₁ L₂ ↦
+      L₁.Forall fun M₁ ↦ L₂.Forall fun M₂ ↦ ¬ permutationsComparison 8 M₁.matroid M₂.matroid :=
+  sorry
