b410f2f5299d73cb77b2b1b8d0a38268856ad4e7
Jakin Chen <jchen612@fordham.edu>
2024-07-03 22:55:04 -0400

    Attempt to prove groupByBucket_lawful (unsuccessful)

--- Matroids/Verification/Buckets.lean
+++ Matroids/Verification/Buckets.lean
@@ -7,5 +7,19 @@ list of list of partial matroids is still a valid (n, r)-sparse paving matroids.
 lemma groupByBucket_lawful (A : List PartialMatroid)
     (hA : A.Forall (fun M ↦ LawfulSparsePavingMatroid n r M.matroid)) :
     (PartialMatroid.groupByBucket A).Forall
-      (fun l ↦ l.Forall (fun M ↦ LawfulSparsePavingMatroid n r M.matroid)) :=
-  sorry
+    (fun l ↦ l.Forall (fun M ↦ LawfulSparsePavingMatroid n r M.matroid)) := by
+  induction A with
+  | nil => simp
+  | cons h t IH =>
+    simp at hA
+    obtain ⟨h_ok, t_ok⟩ := hA
+    apply IH at t_ok
+    simp at IH
+
+
+    obtain ⟨h_ok1, h_ok2, h_ok3, h_ok4, h_ok5⟩ := h_ok
+
+    simp[PartialMatroid.groupByBucket] at *
+    simp[PartialMatroid.findBucket] at *
+    simp[count] at *
+    sorry
