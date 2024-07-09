9024a4636b177596b8fab71fb092e29d048dbe92
Heather Macbeth <25316162+hrmacbeth@users.noreply.github.com>
2024-07-09 15:38:00 -0400

    decide takes care of all of the vamos statements

--- Matroids/Verification/Vamos.lean
+++ Matroids/Verification/Vamos.lean
@@ -1,19 +1,20 @@
 import Matroids.Verification.Basic
 import Matroids.Vamos
 
-lemma vamos_lawful : LawfulSparsePavingMatroid 8 4 Vamos.matroid := sorry
+lemma vamos_lawful : LawfulSparsePavingMatroid 8 4 Vamos.matroid := by decide
 
 lemma vamos_remainingOptions_mem_range :
-    Vamos.remainingOptions.Forall (List.Forall fun i ↦ i < 8) :=
-  sorry
+    Vamos.remainingOptions.Forall (List.Forall fun i ↦ i < 8) := by
+  decide
 
-lemma vamos_remainingOptions_length_eq_rank : Vamos.remainingOptions.Forall (fun l ↦ l.length = 4) :=
-  sorry
+lemma vamos_remainingOptions_length_eq_rank :
+    Vamos.remainingOptions.Forall (fun l ↦ l.length = 4) := by
+  decide
 
 lemma vamos_remainingOptions_sorted_of_mem :
-    Vamos.remainingOptions.Forall fun m ↦ m.Sorted (· < ·) :=
-  sorry
+    Vamos.remainingOptions.Forall fun m ↦ m.Sorted (· < ·) := by
+  decide
 
 lemma vamos_remainingOptions_not_nearlySame :
-    Vamos.matroid.Forall fun l₁ ↦ Vamos.remainingOptions.Forall fun l₂ ↦ ¬ NearlySame l₁ l₂ :=
-  sorry
+    Vamos.matroid.Forall fun l₁ ↦ Vamos.remainingOptions.Forall fun l₂ ↦ ¬ NearlySame l₁ l₂ := by
+  decide
