e18b0ea6f19e9bd222a15f20e6372fa77cec299a
Paulina Delabra Serrano <pau@fordham.edu>
2024-07-16 15:15:16 -0400

    changed elimSmaller to elimGreater

--- Matroids/PartialMatroid.lean
+++ Matroids/PartialMatroid.lean
@@ -11,7 +11,7 @@ any remainng options that become unqualified to be augmented into the partial ma
 
 * `PartialMatroid`: sets up the structure of partial matroid: showing the partial matroid and its
 remaining options.
-* `elimSmaller`: Seeks to make sure that we do not identify partial matroids with different orders
+* `elimGreater`: Seeks to make sure that we do not identify partial matroids with different orders
 but same set of augments from the remaining options as different.
 * `augment`: adds figures to partial matroids and eliminates from remaining options
 -/
@@ -26,19 +26,19 @@ namespace PartialMatroid
 /-- Goes through the remaining options and ensures that we add new things in an increasing
 order Eg. keeps (x,y,z) eliminates (y,x,z). This is used to conserve computing power later on when
 checking for repeat partial matroids-/
-def elimSmaller (l : List Nat) : List (List Nat) → List (List Nat)
+def elimGreater (l : List Nat) : List (List Nat) → List (List Nat)
   | [] => []
   | h1 :: t1 =>
     if h1 > l then
-      elimSmaller l t1
+      elimGreater l t1
     else
-      h1 :: elimSmaller l t1
+      h1 :: elimGreater l t1
 
 /--Adds an additional figure to the partial matroid and then eliminates any figure/option from the
 remaining options that share all or all but one point with the figure that was just added -/
 def augment (l : List Nat) (M : PartialMatroid) : PartialMatroid :=
   -- if !(l ∈ M.remainingOptions) then fail "can't add this" else
-  { matroid := (l :: M.matroid).sort, remainingOptions := elimSmaller l (elimNearlySame l M.remainingOptions) }
+  { matroid := (l :: M.matroid).sort, remainingOptions := elimGreater l (elimNearlySame l M.remainingOptions) }
 
 -- old version with duplication
   -- { matroid := l :: M.matroid, remainingOptions := elimNearlySame l M.remainingOptions }
