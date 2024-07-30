86cf410df7c8a1cd49971d19ded7317081681ac1
Paulina Delabra Serrano <pau@fordham.edu>
2024-07-30 17:15:02 -0400

    created the first NearlySame lemma NearlySame.comm

---
+++ Matroids/Verification/NearlySame.lean
@@ -0,0 +1,6 @@
+import Matroids.NearlySame
+import Matroids.Verification.Basic
+import Matroids.Verification.Miscellaneous
+
+lemma NearlySame.comm {l₁ l₂: List Nat} : NearlySame l₁ l₂ = NearlySame l₂ l₁ := by
+  sorry
