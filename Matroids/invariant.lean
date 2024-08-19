c00611b5d935e9d5ff2146f86c24aaeb5e17c87f
Jakin Chen <jchen612@fordham.edu>
2024-08-19 02:13:48 -0400

    attempting the first four functions in the python script. Including pairCounts, countCounts, and counts.

--- Matroids/invariant.lean
+++ Matroids/invariant.lean
@@ -10,9 +10,22 @@ abbrev c := [0,0,0,0,0,0,0,0]
 
 abbrev out := [0,0,0,0,0,0,0,0]
 
+abbrev outd := [0,0,0,0]
+
+abbrev l := [0,1,2,3,4,5,6,7]
+
 #eval c[2]
 
 -- def counts : (A : List (List Nat)) → List Nat :=
 --   (A.map (·.map (c[·] = c[·] + 1) )).map (out[·] = out[·] + 1)
 
 -- def pairCounts : (A : List (List (List Nat))) → List Nat :=
+--   A.map( (c[·[l.map(·)]][·[l.map(·+1)]]) = (c[·[l.map(·)]][·[l.map(·+1)]]) + 1 )
+
+-- def countCounts : (A : List (List Nat)) → List Nat :=
+--   l.map(out[(pairCounts A)[·][l.map(· + 1)]] )
+
+
+/--I am unsure how to tell it to return nothing if i is in L-/
+-- def compl : (A : List Nat) → Nat :=
+--   l.map (if !(list.mem · A) then i)
